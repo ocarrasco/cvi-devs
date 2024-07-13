bb538ef80f4db69e33c5ebad545f07f32da715e5
Sheposai <emiliosepulveda.ss98@gmail.com>
2024-07-13 14:34:54 -0400

    first commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,6 +8,9 @@ public class Main {
   }
 
   public static double divide(int n1, int n2) {
+    if (n1 == 0) {
+      throw new RuntimeException("Nop se pede dividir por cero");
+    }
     return n1/n2;
   }
 
